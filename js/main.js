281d4e4f78b1db2f1219c911b187b31e962aaff6
Valerio <canivalerio93@gmail.com>
2022-07-13 16:01:53 +0200

    inizializzazione progetto

---
+++ js/main.js
@@ -0,0 +1,59 @@
+// let i=0;
+
+// let number;
+
+// let somma=0;
+
+// while(i < 3){
+//     number = parseInt(prompt("inserisci un numero"));
+//     somma += number;
+//     i++;
+// };
+
+// alert(somma)
+
+
+// let i=0 ; 
+// const listaNomi = ["Alberto","Beatrice","Carlo","Denise","Erica","Federico"];
+// const invitati = listaNomi.length;
+// const nameInput = prompt('digita il tuo Nome');
+
+
+// while(i<invitati && nameInput!==listaNomi[i-1]){
+//     if( nameInput == listaNomi[i]){
+//         alert('lei compare tra gli invitati');
+//         i++;
+//     } else if(i == invitati-1){
+//         alert('lei non compare tra gli invitati');
+//         i++;
+//     } else {
+//         i++;
+//     }
+// }
+
+// let i=1;
+// let somma=0;
+
+// while(i <= 10){
+   
+//     somma += i;
+//     i++;
+// };
+// let media = somma / 10;
+// alert(somma);
+//alert(media);
+
+// let i=1;
+// let somma=0;
+// const x = parseInt(prompt("inserisci fino a che numero fare somma e media"));
+
+// while(i <= x){
+   
+//     somma += i;
+//     i++;
+// };
+
+// let media = somma / x;
+
+// alert(somma);
+// alert(media);
